75c943f17c85ed5e8295f1d04707455a637509dd
bepicard <benjamin.picard@etu.uca.fr>
2018-12-19 11:06:12 +0100

    collision entre voiture fonctionne

--- FroggerJavaFX/src/Vue/MenuController.java
+++ FroggerJavaFX/src/Vue/MenuController.java
@@ -1,24 +1,30 @@
 package Vue;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class MenuController {
 
+    @FXML
+    ChoiceBox lvl;
+
     public void buttonJouer(ActionEvent actionEvent) {
         Stage primaryStage = (Stage)((Button) actionEvent.getSource()).getScene().getWindow();
         Plateau p = new Plateau();
+
+        p.lvlProperty().bind(lvl.valueProperty());
         try {
             p.start(primaryStage);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
-
 }
--- FroggerJavaFX/src/Vue/Plateau.java
+++ FroggerJavaFX/src/Vue/Plateau.java
@@ -2,6 +2,8 @@ package Vue;
 
 import Controller.Controller;
 import Model.*;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -18,12 +20,16 @@ public class Plateau {
     private ApplicationMediator mediator;
     private Barman barman;
 
+    private ObjectProperty lvl = new SimpleObjectProperty();
+    //public final Object getLvl() { return lvl.get(); }
+    //public final void setLvl(String value) { lvl.set(value); }
+    public ObjectProperty lvlProperty() { return lvl; }
+
     public Plateau() {
         this.root = new Group();
         this.scene = new Scene(root, 1280, 720);
         this.controller = new Controller();
         this.mediator = new ApplicationMediator();
-        this.barman = new Barman(mediator, scene.getWidth()/2-25, scene.getHeight()-50, 50, 50, 0.5, "images/loic.png");
     }
 
     public void start(Stage primaryStage) throws Exception {
@@ -35,12 +41,32 @@ public class Plateau {
         ImageView background = new ImageView(new Image(new FileInputStream("images/fond.png")));
         root.getChildren().add(background);
         controller.setEvenementClavier(scene);
-        mediator.ajouterEntite(barman);
-        root.getChildren().add(barman);
-        controller.updateBarman(scene, barman);
 
-        Niveau facile = new Niveau(4, 0, 2, 3);
-        loadLevel(facile);
+        switch((String)lvlProperty().get()) {
+            case "Facile":
+                System.out.println("Facile");
+                Niveau facile = new Niveau(1, 4, 0.5, 1, 0, 2, 3);
+                loadLevel(facile);
+                break;
+            case "Normal":
+                System.out.println("Normal");
+                Niveau normal = new Niveau(0.7, 6, 0.7, 1.5, 0, 2, 3);
+                loadLevel(normal);
+                break;
+            case "Hardcore":
+                System.out.println("Difficile");
+                Niveau hardcore = new Niveau(0.5, 8, 1, 2, 0, 2, 3);
+                loadLevel(hardcore);
+                break;
+            case "Bouré":
+                System.out.println("Bouré");
+                Niveau boure = new Niveau(0.2, 8, 1.5, 2.5, 0, 2, 3);
+                loadLevel(boure);
+                break;
+        }
+
+
+
 
 
         /*Boisson biere = new Boisson("biere");
@@ -60,13 +86,16 @@ public class Plateau {
 
     public void loadLevel(Niveau niveau) {
         Random r = new Random();
+        double min = niveau.getMinSpeed();
+        double max = niveau.getMaxSpeed();
+
         int index = 1;
         for(int i = 1; i <= niveau.getNbVoitures(); i++) {
             Voiture v;
             if(index == 1 || index == 2) {
-                v = new Voiture(mediator, i * 200, 125 * index, 200, 100, 1 + (1.3 - 0.3) * r.nextDouble(),"LEFT", "images/voiture" + index + ".png");
+                v = new Voiture(mediator, i * 250, 125 * index, 200, 100, min + (max - min) * r.nextDouble(),"LEFT", "images/voiture" + index + ".png");
             } else {
-                v = new Voiture(mediator, i * 200, 125 * index, 200, 100, 1 + (1.3 - 0.3) * r.nextDouble(), "RIGHT", "images/voiture" + index + ".png");
+                v = new Voiture(mediator, i * 250, 125 * index, 200, 100, min + (max - min) * r.nextDouble(), "RIGHT", "images/voiture" + index + ".png");
             }
             niveau.ajouterVoiture(v);
             mediator.ajouterEntite(v);
@@ -78,7 +107,13 @@ public class Plateau {
                 index = 1;
             }
         }
-        controller.collisionEntreVoiture(scene, niveau.getVoitures());
+        this.barman = new Barman(mediator, scene.getWidth()/2-25, scene.getHeight()-50, 50, 50, niveau.getSpeedBarman(), "images/loic.png");
+        mediator.ajouterEntite(barman);
+        root.getChildren().add(barman);
+        controller.updateBarman(scene, barman);
+
+
+        controller.collisionEntreVoiture(scene, niveau.getVoitures(), (max-min)/2, max, min);
         controller.collisionObstacle(scene, barman);
     }
 
