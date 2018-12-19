b7c72b751eda65acfb45911b740e49a8efbab24c
leseguin <leane.seguin@etu.uca.fr>
2018-12-19 11:35:37 +0100

    ajout de la creation des clients, de leur placement et des deplacements

--- FroggerJavaFX/src/Model/Mediator.java
+++ FroggerJavaFX/src/Model/Mediator.java
@@ -1,6 +1,9 @@
 package Model;
 
 public interface Mediator {
+    void ajouterEntite(Entite e);
+    void enleverEntite(Entite e);
+    Entite collisionEntite(Entite e);
     Entite collisionVoiture(Entite e);
     Entite collisionBar(Entite e);
     Entite collisionClient(Entite e);
