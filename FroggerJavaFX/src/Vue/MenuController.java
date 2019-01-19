1d7ee11990f9f3e9a9685516d4b2ab30cb37be8f
leseguin <leane.seguin@etu.uca.fr>
2019-01-19 18:30:41 +0100

    Ajout des regles du jeu

--- FroggerJavaFX/src/Vue/MenuController.java
+++ FroggerJavaFX/src/Vue/MenuController.java
@@ -7,13 +7,14 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -58,6 +59,18 @@ public class MenuController implements Initializable {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+   public void buttonRegles(ActionEvent actionEvent){
+        Stage stageRegles = new Stage();
+        try {
+            Scene sceneRegles = new Scene(FXMLLoader.load(getClass().getResource("Regles.fxml")));
+            stageRegles.setScene(sceneRegles);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stageRegles.initModality(Modality.APPLICATION_MODAL);
+        stageRegles.show();
     }
 
 
